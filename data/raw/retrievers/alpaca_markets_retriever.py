68365a2df4c379f8230e7dbddce1a4514c2dd44d
Ivan Kurnosau <ikurnosau@deloitte.com>
2025-08-12 18:36:02 +0200

    new download file

--- data/raw/retrievers/alpaca_markets_retriever.py
+++ data/raw/retrievers/alpaca_markets_retriever.py
@@ -36,14 +36,14 @@ def _download_file_from_gdrive(file_id: str, output_dir: str, file_name: str):
 
 def _download_from_gdrive():
     _download_file_from_gdrive(
-        file_id= "1oE69lvgomUzIqJHCOJ4N1g6opWuv1coW",
+        file_id= "1ofgmKnmpe-sxbyHCcm_Vg8J6tQ9ynWnv",
         output_dir='../data/raw/alpaca/bars_with_quotes',
-        file_name="1Min_2024-06-01-2025-06-01_AAPL+AMD+BABA+BITU+C+CSCO+DAL+DIA+GLD+GOOG+IJR+MARA+MRVL+MU+NEE+NKE+NVDA+ON+PLTR+PYPL+QLD+QQQ+QQQM+R.pkl"
+        file_name="1Hour_2022-01-01-2025-01-01_AAPL+AMD+BABA+BITU+C+CSCO+DAL+DIA+GLD+GOOG+IJR+MARA+MRVL+MU+NEE+NKE+NVDA+ON+PLTR+PYPL+QLD+QQQ+QQQM+R.pkl"
     )
     # _download_file_from_gdrive(
-    #     file_id= "1Vk2sviBbB0srSiq_OsHsnYGVlG-BryBN",
+    #     file_id= "1oE69lvgomUzIqJHCOJ4N1g6opWuv1coW",
     #     output_dir='../data/raw/alpaca/bars_with_quotes',
-    #     file_name="1Min_2016-01-01-2018-01-01_MMM+AXP+AMGN+AMZN+AAPL+BA+CAT+CVX+CSCO+KO+DIS+GS+HD+HON+IBM+JNJ+JPM+MCD+MRK+MSFT+NKE+NVDA+PG+CRM+SHW.pkl"
+    #     file_name="1Min_2024-06-01-2025-06-01_AAPL+AMD+BABA+BITU+C+CSCO+DAL+DIA+GLD+GOOG+IJR+MARA+MRVL+MU+NEE+NKE+NVDA+ON+PLTR+PYPL+QLD+QQQ+QQQM+R.pkl"
     # )
     _download_file_from_gdrive(
         file_id= "1fBIwQMGOf-cV5IN-psvWqSV2I3MvPum_",
